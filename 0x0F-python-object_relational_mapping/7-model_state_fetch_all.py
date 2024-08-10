e986f3ac60c01e0f8684104b78354c7d6fe262cd
mo7amed3mad17 <memo12317emad@gmail.com>
2024-08-11 01:04:47 +0200

    get

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Lists all State objects from the database hbtn_0e_6_usa."""
+
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Get the database connection parameters from the command line
+    un = sys.argv[1]
+    pw = sys.argv[2]
+    dn = sys.argv[3]
+
+    # Create the SQLAlchemy engine
+    engine = create_engine(f'mysql://{un}:{pw}@localhost:3306/{dn}')
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the State objects and sort them by id
+    states = session.query(State).order_by(State.id).all()
+
+    # Print the results
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()

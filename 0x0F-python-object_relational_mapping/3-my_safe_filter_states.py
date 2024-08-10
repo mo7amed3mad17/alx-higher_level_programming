5a35543589961e5e0d330c4f4860bb599ebd76a7
mo7amed3mad17 <memo12317emad@gmail.com>
2024-08-10 02:19:59 +0200

    go

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    # Create a cursor object
+    c = db.cursor()
+
+    # Execute the query to fetch all states
+    name = sys.argv[4]
+    c.execute("SELECT * FROM states WHERE name LIKE %s", (name, ))
+
+    # Fetch and print the results
+    for row in c.fetchall():
+        print(row)
+
+    # Close the cursor and the database connection
+    c.close()
+    db.close()
